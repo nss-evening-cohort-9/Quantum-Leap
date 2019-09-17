0e3a5c13fd1a761b1b871f7c825b26df79a20f79
Sean Davis <sean.dev.davis@gmail.com>
2019-09-17 18:16:15 -0500

    setup

---
+++ QuantumLeap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuantumLeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
