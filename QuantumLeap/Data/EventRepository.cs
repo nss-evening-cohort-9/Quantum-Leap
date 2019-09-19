593490c90a68c6439a9f9e329d8c397be991d004
Andrew Tiller <tiller.andrew@gmail.com>
2019-09-18 20:48:32 -0500

    Added GetHostById method. Added Event class and EventRepository content. Added demo code to Program to show that you can get a random element from a repo, take its ID, pass it back into the Get[...]ById and get the same element back

--- QuantumLeap/Data/EventRepository.cs
+++ QuantumLeap/Data/EventRepository.cs
@@ -9,7 +9,29 @@ namespace QuantumLeap.Data
     {
         static readonly List<Event> _events = new List<Event>
         {
-            new Event
+            new Event(new DateTime(2001, 09, 11), "New York"),
+            new Event(new DateTime(1944, 06, 06), "Normandy"),
+            new Event(new DateTime(2007, 09, 25), "Gamestop")
+        };
+
+        readonly Random random = new Random();
+
+        public List<Event> GetAll()
+        {
+            return _events;
+        }
+
+        public Event GetEventById(Guid id)
+        {
+            Event requestedEvent = _events.Find(x => x.Id == id);
+            return requestedEvent;
+        }
+
+        public Event GetRandom()
+        {
+            int randomIndex = random.Next(0, _events.Count);
+            Event randomEvent = _events[randomIndex];
+            return randomEvent;
         }
     }
 }
